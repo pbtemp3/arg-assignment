a4ec23ccd5153771a8296242e0445b2fbfa976f7
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2024-02-12 13:59:21 +0530

    added missing id's

--- modules/book/book.util.js
+++ modules/book/book.util.js
@@ -1,5 +1,6 @@
 const BOOKS_API_STATIC_DATA = [
   {
+    id: 1,
     name: "To Kill a Mockingbird",
     description:
       "A Pulitzer Prize-winning novel set in the American South during the 1930s, addressing racial injustice and moral growth through the eyes of a young girl.",
@@ -8,6 +9,7 @@ const BOOKS_API_STATIC_DATA = [
     publisher: "Harper Perennial Modern Classics",
   },
   {
+    id: 2,
     name: "1984",
     description:
       "A dystopian novel set in a totalitarian regime where critical thought is suppressed, and individualism is forbidden.",
@@ -16,6 +18,7 @@ const BOOKS_API_STATIC_DATA = [
     publisher: "Secker & Warburg",
   },
   {
+    id: 3,
     name: "The Great Gatsby",
     description:
       "A classic American novel depicting the Jazz Age and the American Dream through the eyes of narrator Nick Carraway.",
